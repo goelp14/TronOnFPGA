1e7864379ec72b41a1f8d8ab77bba1229d4f0e43
Ben Xie <bx3@illinois.edu>
2020-04-18 21:39:20 -0400

    worked on movement system

--- Project_Files/arena.sv
+++ Project_Files/arena.sv
@@ -14,28 +14,36 @@
 //-------------------------------------------------------------------------
 
 
-module  ball ( input         Clk,                // 50 MHz clock
+module  arena ( input         Clk,                // 50 MHz clock
                              Reset,              // Active-high reset signal
                              frame_clk,          // The clock indicating a new frame (~60Hz)
+					input 		  collision // signals to know if red/blue have collided into the trail
 					input [7:0]   keycode,
-               input [9:0]   DrawX, DrawY,       // Current pixel coordinates
-               output logic  is_ball             // Whether current pixel belongs to ball or background
+               output logic [9:0] Blue_X, Blue_Y, Red_X, Red_Y,
+					output logic [1:0] Blue_dir, Red_dir,
               );
     
-    parameter [9:0] Ball_X_Center = 10'd320;  // Center position on the X axis
-    parameter [9:0] Ball_Y_Center = 10'd240;  // Center position on the Y axis
-    parameter [9:0] Ball_X_Min = 10'd0;       // Leftmost point on the X axis
-    parameter [9:0] Ball_X_Max = 10'd639;     // Rightmost point on the X axis
-    parameter [9:0] Ball_Y_Min = 10'd0;       // Topmost point on the Y axis
-    parameter [9:0] Ball_Y_Max = 10'd479;     // Bottommost point on the Y axis
-    parameter [9:0] Ball_X_Step = 10'd1;      // Step size on the X axis
-    parameter [9:0] Ball_Y_Step = 10'd1;      // Step size on the Y axis
-    parameter [9:0] Ball_Size = 10'd4;        // Ball size
-    
-    logic [9:0] Ball_X_Pos, Ball_X_Motion, Ball_Y_Pos, Ball_Y_Motion;
-    logic [9:0] Ball_X_Pos_in, Ball_X_Motion_in, Ball_Y_Pos_in, Ball_Y_Motion_in;
-    
-    //////// Do not modify the always_ff blocks. ////////
+    parameter [9:0] Arena_X_Spawn_blue = 10'd150;  // Center position on the X axis
+	 parameter [9:0] Arena_X_Spawn_red = 10'd490;  // Center position on the X axis
+    parameter [9:0] Arena_Y_Spawn = 10'd240;  // Center position on the Y axis
+    parameter [9:0] Arena_X_Min = 10'd0;       // Leftmost point on the X axis
+    parameter [9:0] Arena_X_Max = 10'd639;     // Rightmost point on the X axis
+    parameter [9:0] Arena_Y_Min = 10'd0;       // Topmost point on the Y axis
+    parameter [9:0] Arena_Y_Max = 10'd479;     // Bottommost point on the Y axis
+    parameter [9:0] Bike_X_Step = 10'd1;      // Step size on the X axis
+    parameter [9:0] Bike_Y_Step = 10'd1;      // Step size on the Y axis
+   
+	 // logic to reset players+board without resetting score
+	 logic reset_arena;
+	 
+	 logic [9:0] Blue_X, Blue_Y, Blue_X_M, Blue_Y_M, Blue_Y_M, Red_X, Red_Y, Red_X_M, Red_Y_M, Red_Y_M;
+	 logic [9:0] Blue_X_new, Blue_Y_new, Blue_X_M_new, Blue_Y_M_new, Blue_Y_M_new, Red_X_new, Red_Y_new, Red_X_M_new, Red_Y_M_new, Red_Y_M_new;
+	 
+	 // logic to control which direction sprite is used
+	 logic [1:0] Blue_dir, Red_dir, Blue_dir_new, Red_dir_new;
+	 enum logic [1:0] {up, down, left, right};
+	 
+
     // Detect rising edge of frame_clk
     logic frame_clk_delayed, frame_clk_rising_edge;
     always_ff @ (posedge Clk) begin
@@ -45,31 +53,57 @@ module  ball ( input         Clk,                // 50 MHz clock
     // Update registers
     always_ff @ (posedge Clk)
     begin
-        if (Reset)
+        if (Reset || reset_arena)
         begin
-            Ball_X_Pos <= Ball_X_Center;
-            Ball_Y_Pos <= Ball_Y_Center;
-            Ball_X_Motion <= 10'd0;
-            Ball_Y_Motion <= Ball_Y_Step;
+				// for blue bike
+            Blue_X <= Arena_X_Spawn_blue;
+            Blue_Y <= Arena_Y_Spawn;
+            Blue_X_M <= Bike_X_Step;
+            Blue_Y_M <= 10'd0;
+				Blue_dir <= right;
+				
+				// for red bike
+            Red_X <= Arena_X_Spawn_red;
+            Red_Y <= Arena_Y_Spawn;
+            Red_X_M <= ~(Bike_X_Step) + 1'b1;
+            Red_Y_M <= 10'd0;
+				Red_dir <= left;		
         end
         else
         begin
-            Ball_X_Pos <= Ball_X_Pos_in;
-            Ball_Y_Pos <= Ball_Y_Pos_in;
-            Ball_X_Motion <= Ball_X_Motion_in;
-            Ball_Y_Motion <= Ball_Y_Motion_in;
+				// for blue bike
+            Blue_X <= Blue_X_new;
+            Blue_Y <= Blue_Y_new;
+            Blue_X_M <= Blue_X_M_new;
+            Blue_Y_M <= Blue_Y_M_new;
+				Blue_dir <= Blue_dir_new;
+				
+				// for blue bike
+            Red_X <= Red_X_new;
+            Red_Y <= Red_Y_new;
+            Red_X_M <= Red_X_M_new;
+            Red_Y_M <= Red_Y_M_new;
+				Red_dir <= Red_dir_new;
         end
     end
-    //////// Do not modify the always_ff blocks. ////////
+
     
     // You need to modify always_comb block.
     always_comb
     begin
         // By default, keep motion and position unchanged
-        Ball_X_Pos_in = Ball_X_Pos;
-        Ball_Y_Pos_in = Ball_Y_Pos;
-        Ball_X_Motion_in = Ball_X_Motion;
-        Ball_Y_Motion_in = Ball_Y_Motion;
+		  // Blue
+		  Blue_X_new = Blue_X
+		  Blue_Y_new = Blue_Y
+		  Blue_X_M_new = Blue_X_M
+		  Blue_Y_M_new = Blue_Y_M
+		  Blue_dir_new = Blue_Dir
+		  //Red
+		  Red_X_new = Red_X
+		  Red_Y_new = Red_Y
+		  Red_X_M_new = Red_X_M
+		  Red_Y_M_new = Red_Y_M
+		  Red_dir_new = Red_Dir
         
         // Update position and motion only at rising edge of frame clock
         if (frame_clk_rising_edge)
