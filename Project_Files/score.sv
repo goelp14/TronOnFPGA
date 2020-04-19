3e7b8b1ff7363d636c919f0a07eb09de7a37b6b1
Ben Xie <bx3@illinois.edu>
2020-04-18 22:10:23 -0400

    more work done, added more modules

--- Project_Files/score.sv
+++ Project_Files/score.sv
@@ -1,3 +1,12 @@
-module score ();
+// NOTES:
+// Not sure what to output
+
+module score ( input         Clk,                // 50 MHz clock
+                             Reset_Score,        // Active-high reset signal
+                             frame_clk,          // The clock indicating a new frame (~60Hz)
+									  
+					input logic collision_blue, collision_red,
+               output logic 
+);
 
 endmodule
--- Project_Files/trails.sv
+++ Project_Files/trails.sv
@@ -1,3 +1,11 @@
-module trails ();
+// this module saves where the trails are in the world, and also detects collisions
+
+module trails ( input        Clk,                // 50 MHz clock
+                             Reset,              // Active-high reset signal
+                             frame_clk,          // The clock indicating a new frame (~60Hz)
+					output logic collision_blue, collision_red,
+               input logic [9:0] Blue_X, Blue_Y, Red_X, Red_Y,
+					input logic [1:0] Blue_dir, Red_dir
+					);
 
 endmodule
