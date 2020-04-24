37a60832ecc9da3069e899b465221cf25a930583
Pranav Goel <pranavgoel11@gmail.com>
2020-04-24 00:30:27 -0500

    fix qsys

--- Project_Files/lab8_soc/synthesis/submodules/lab8_soc_sysid_qsys_0.v
+++ Project_Files/lab8_soc/synthesis/submodules/lab8_soc_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module lab8_soc_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1585263302 : 0;
+  assign readdata = address ? 1587705873 : 0;
 
 endmodule
 
