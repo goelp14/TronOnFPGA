9db6da446e8e340f4c671a6b73b95c2ba84860b6
Pranav Goel <pranavgoel11@gmail.com>
2020-04-23 22:32:06 -0500

    sram beginning

---
+++ Project_Files/Mem2IO.sv
@@ -0,0 +1,36 @@
+//-------------------------------------------------------------------------
+//      Mem2IO.vhd                                                       --
+//      Stephen Kempf                                                    --
+//                                                                       --
+//      Revised 03-15-2006                                               --
+//              03-22-2007                                               --
+//              07-26-2013                                               --
+//              03-04-2014                                               --
+//              02-13-2017                                               --
+//                                                                       --
+//      For use with ECE 385 Experiment 6                                --
+//      UIUC ECE Department                                              --
+//-------------------------------------------------------------------------
+
+
+module  Mem2IO ( 	input logic Clk, Reset,
+					input logic [19:0]  ADDR, 
+					input logic CE, UB, LB, OE, WE,
+					input logic [15:0]  Frame_Buffer, Menu, Background,
+					input logic [15:0] Data_from_REG, Data_from_SRAM,
+					output logic [15:0] Data_to_REG, Data_to_SRAM
+					);
+
+	logic [15:0] hex_data;
+   
+	// Load data from switches when address is xFFFF, and from SRAM otherwise.
+	always_comb
+    begin 
+        Data_to_REG = 16'd0;
+        if (WE && ~OE)
+			Data_to_REG = Data_from_SRAM;
+    end
+
+    // Pass data from CPU to SRAM
+	assign Data_to_SRAM = Data_from_REG;
+endmodule
