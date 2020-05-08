3fd3a248d4451b9b09c5a8f77c9ce09fbb85c21c
Pranav Goel <pranavgoel11@gmail.com>
2020-05-08 09:04:55 -0500

    def right

--- Final_Project_Project_Files/SVFiles/combine.sv
+++ Final_Project_Project_Files/SVFiles/combine.sv
@@ -5,8 +5,11 @@ module combine(
 	input [3:0] Data_In_Bike,
 	input [18:0] write_address,
 	input [1:0] r_or_b,
+	input [9:0] Blue_X_real, Blue_Y_real, Red_X_real, Red_Y_real,
+	input [1:0] Blue_dir, Red_dir,
    output logic [3:0]  color_enum,
-	output logic [7:0] red_color, blue_color 
+	output logic [7:0] red_color, blue_color,
+	output logic [15:0] dOut
     );
 
 
@@ -17,6 +20,63 @@ module combine(
 	logic [3:0]  out_byte;
 	
 	logic bool;
+	
+//	parameter [9:0] Bike_Size = 10'd16;
+//	
+//	int DistX_blue, DistY_blue;
+//   assign DistX_blue = DrawX - Blue_X_real - Bike_Size;
+//	assign DistY_blue = DrawY - Blue_Y_real + Bike_Size;
+//	
+//	int DistX_red, DistY_red;
+//   assign DistX_red = DrawX - Red_X_real - Bike_Size;
+//	assign DistY_red = DrawY - Red_Y_real + Bike_Size;
+//	
+//	always_comb
+//		begin
+//			// for blue
+//			if (Blue_dir == 2'b00)
+//				begin
+//					tempoffsetbluex = 10'd0;
+//					tempoffsetbluey = 10'd4;
+//				end
+//			else if (Blue_dir == 2'b01)
+//				begin
+//					tempoffsetbluex = 10'd0;
+//					tempoffsetbluey = -10'd4;
+//				end
+//			else if (Blue_dir == 2'b10)
+//				begin
+//					tempoffsetbluex = 10'd4;
+//					tempoffsetbluey = 10'd0;
+//				end
+//			else
+//				begin
+//					tempoffsetbluex = -10'd4;
+//					tempoffsetbluey = 10'd0;
+//				end	
+//				
+//			if (Red_dir == 2'b00)
+//				begin
+//					tempoffsetredx = 10'd0;
+//					tempoffsetredy = 10'd4;
+//				end
+//			else if (Red_dir == 2'b01)
+//				begin
+//					tempoffsetredx = 10'd0;
+//					tempoffsetredy = -10'd4;
+//				end
+//			else if (Red_dir == 2'b10)
+//				begin
+//					tempoffsetredx = 10'd4;
+//					tempoffsetredy = 10'd0;
+//				end
+//			else
+//				begin
+//					tempoffsetredx = -10'd4;
+//					tempoffsetredy = 10'd0;
+//				end	
+//		end
+		
 	
 //	assign bool = data_Out [3:0] != 4'h08 || data_Out [11:8] != 4'h08 || data_Out [3:0] != 4'h00 || data_Out [11:8] != 4'h00;	
 	always_comb begin
@@ -33,22 +93,79 @@ module combine(
 		else
 			begin
 				read_address = DrawX/2 + DrawY * (640/2);
+
+//				if (Blue_dir == 2'b00)
+//					begin
+//						if ( (DistX_blue <= Bike_Size * 2) && (DistY_blue <= Bike_Size - 1) )
+//							begin
+////								read_address = DrawX/2 + DrawY * (640/2);
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									blue = 8'b0;
+//							end
+//					end
+//				else if (Blue_dir == 2'b01)
+//					begin
+//						if ( (DistX_blue <= Bike_Size * 2) && (DistY_blue >= Bike_Size + 1) )
+//							begin
+////								read_address = DrawX/2 + DrawY * (640/2);
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									blue = 8'b0;
+//							end
+//					end
+//				else if (Blue_dir == 2'b10)
+//					begin
+//						if ( (DistX_blue <= Bike_Size) && (DistY_blue <= Bike_Size * 2) )
+//							begin
+////								read_address = DrawX/2 + DrawY * (640/2);
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									blue = 8'b0;
+//							end
+//					end
+//				else
+//					begin
+//						if ( (DistX_blue >= Bike_Size) && (DistY_blue <= Bike_Size * 2) )
+//							begin
+////								read_address = DrawX/2 + DrawY * (640/2);
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									blue = 8'b0;
+//							end
+//					end
+//				if (r_or_b == 2'b00)
+//					begin
+//						if ((offsetbx == tempoffsetbluex) && (offsetby = tempoffsetbluey))
+//							begin
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									blue = 8'b1;
+//							end
+//					end
+//				else if (r_or_b == 2'b01)
+//					begin
+//						if ((offsetrx == tempoffsetredx) && (offsetry = tempoffsetredy))
+//							begin
+//								if (data_Out[3:0] != 4'h08 || data_Out[11:8] != 4'h08)
+//									red = 8'b1;
+//							end
+//					end
 				if (r_or_b == 2'b00)
 					begin
-						if(data_Out [3:0] != 4'h08 || data_Out [11:8] != 4'h08)
-							begin
-								if(data_Out [3:0] != 4'h04 || data_Out [11:8] != 4'h04)
-									blue = 8'b0;
-							end
+//						if(data_Out [3:0] == 4'h08 || data_Out [11:8] == 4'h08)
+//							blue = 1'b1;
+						if (data_Out [3:0] == 4'h06 || data_Out [11:8] == 4'h06)
+							blue = 8'b0;
+						else if (data_Out [3:0] == 4'h0e || data_Out [11:8] == 4'h0e)
+							blue = 8'b0;
+//						else
+							
 						
 					end
 				else if (r_or_b == 2'b01)
 					begin
-						if(data_Out [3:0] != 4'h08 || data_Out [11:8] != 4'h08)
-							begin
-								if(data_Out [3:0] != 4'h06 || data_Out [11:8] != 4'h06)
-									red = 8'b0;
-							end
+//						if(data_Out [3:0] != 4'h08 && data_Out [11:8] != 4'h08)
+//							red = 8'b0;
+						if(data_Out [3:0] == 4'h04 || data_Out [11:8] == 4'h04)
+							red = 8'b0;
+						else if (data_Out [3:0] == 4'h0e || data_Out [11:8] == 4'h0e)
+							red = 8'b0;
 					end
 				out_byte = Data_In_Bike;
 			end
@@ -58,5 +175,6 @@ module combine(
 	assign color_enum = out_byte;
 	assign red_color = red;
 	assign blue_color = blue;
+	assign dOut = data_Out;
 
 endmodule
