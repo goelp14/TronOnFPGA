5d0addb29211883d72ec043962f26215ca95d169
Pranav Goel <pranavgoel11@gmail.com>
2020-05-08 18:55:12 -0500

    stuff

--- Final_Project_Project_Files/SVFiles/gamestate.sv
+++ Final_Project_Project_Files/SVFiles/gamestate.sv
@@ -71,23 +71,21 @@ module GameState (   input logic  Clk, Reset, Reset_Game, Reset_Round, Blue_W, R
 					Next_state = Round_Started;
 			Round_Started :
 				begin
-//					if (Blue_W)
-//						begin
-//							Next_state = Blue_Wins;
-//							load_background = 1'b1;
-//						end
-//					else if (Red_W)
-//						begin
-//							Next_state = Red_Wins;
-//							load_background = 1'b1;
-//						end
-//					else if (Reset_Round)
-//						begin
-//							Next_state = Round_Paused;
-//							load_background = 1'b1;
-//						end
-					Next_state = Blue_Wins;
-					load_background = 1'b1;
+					if (Blue_W)
+						begin
+							Next_state = Blue_Wins;
+							load_background = 1'b1;
+						end
+					else if (Red_W)
+						begin
+							Next_state = Red_Wins;
+							load_background = 1'b1;
+						end
+					else if (Reset_Round)
+						begin
+							Next_state = Round_Paused;
+							load_background = 1'b1;
+						end
 				end
 			Blue_Wins :
 				if (keycode == 8'h28)
